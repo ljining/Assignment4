562c633235486e3b38834ca38f535f5833d78ad5
ljining <108book@naver.com>
2024-04-16 16:36:14 +0900

    Initial Commit

---
+++ WishList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WishList
+//
+//  Created by 이유진 on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
